a1d9af95c23d3faeeefc9d6057502695735b29a4
Sergei Krainov <skrajnov1992@gmail.com>
2022-07-11 14:12:34 +0300

    Photo sending

--- Messenger/Resourses/StorageManager.swift
+++ Messenger/Resourses/StorageManager.swift
@@ -41,6 +41,31 @@ final class StorageManager {
         })
         
     }
+    /// Upload image that will be send in a converastion message
+    public func uploadMessagePhoto(with data: Data, fileName: String, complition: @escaping UploadPictureComplition) {
+        
+        storage.child("message_images/\(fileName)").putData(data, metadata: nil, completion: {metadata, error in
+            guard error == nil else {
+                //failed
+                print("failed to upload data to firebase for picture")
+                complition(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            
+            self.storage.child("message_images/\(fileName)").downloadURL(completion: {url, error in
+                guard let url = url else {
+                    print("failed to get dowload url")
+                    complition(.failure(StorageErrors.failedToGetDowloadUrl))
+                    return
+                }
+                
+                let urlString = url.absoluteString
+                print("dowload url returned: \(urlString)")
+                complition(.success(urlString))
+            })
+        })
+        
+    }
     
     public enum StorageErrors: Error {
         case failedToUpload
